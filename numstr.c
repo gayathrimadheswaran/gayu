e5342bf14a847d4ddf85da09c7a8c0e84f2ab9a4
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-31 16:44:24 +0530

    numstr.c

---
+++ numstr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("\n string numeric");
+}
+else
+{
+printf("\n not a numeric);
+}
+return 0;
+}
