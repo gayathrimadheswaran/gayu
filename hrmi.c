358f0688b324c61a22680728282127ba3d5d2e65
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-31 16:55:17 +0530

    hrmi.c

---
+++ hrmi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int h1,h2,m1,m2,h,m;
+printf("\n enter h1 val:");
+scanf("%d",&h1);
+printf("\n enter h2 val:");
+scanf("%d",&h2);
+h=h1-h2;
+m=m1-m2;
+printf("\n the result is %d %d",h,m);
+return 0;
+}
