a4680738ff17e3d882752718b949e420817a7aa6
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-01-31 16:02:28 +0530

    number.c

---
+++ number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,n1,n2,n3,n4,n5;
+printf("enter the number");
+scanf("%d",&n);
+n1=n*1;
+n2=n*2;
+n3=n*3;
+n4=n*4;
+n5=n*5;
+printf("the result is  %d%d%d%d%d");
+return 0;
+}
