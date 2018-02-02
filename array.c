c28121d557e659724414ead0697dc27dacfb625c
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-02 16:05:48 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b[15],i;
+	printf("\nNUMBER");
+	scanf("%d",&a);
+	printf("\n ARRAY ELMTS");
+	for(i=0;i<=a;i++)
+	{
+		scanf("%d",&b[15]);
+		printf("\n the result is %d %d", i,b[15]);
+	}
+	return 0;
+}
