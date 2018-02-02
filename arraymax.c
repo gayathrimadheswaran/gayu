26d948e95843d363fa72236df7142cacd29eb303
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-02 16:30:26 +0530

    arraymax.c

---
+++ arraymax.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b[15],i,c;
+	printf("\narray size");
+	scanf("%d",&a);
+	printf("\nARRAY ELMTS");
+	for(i=0;i<=a;i++)
+	{
+		scanf("%d",&b[15]);
+	}
+	if(b[15]>a)
+	{
+		c=b[15];
+	}
+	printf("\n result is %d",c);
+	return 0;
+}
