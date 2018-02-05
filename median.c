0e2e70cb41cfb64f5a25c87257b7982d9b146478
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-05 16:11:45 +0530

    median.c

---
+++ median.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b[5],c,d,i;
+	printf("value");
+	scanf("%d",&a);
+	printf("\narray elmts");
+	for(i=0;i<=a;i++)
+	{
+		scanf("%d",&b[5]);
+	//	c=b[5]+b[5]+b[5];
+	//	d=c/3;
+		printf("\n%d %d", c,d);
+	}
+	return 0;
+}
