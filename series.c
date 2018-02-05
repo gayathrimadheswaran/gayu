f51cc9f5fd101c78e22c04a2f3c264021ddc4ac7
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-05 16:24:20 +0530

    series.c

---
+++ series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n1,a=0,b=1,c;
+	printf("value");
+	scanf("%d",&n1);
+	for(i=1;i<n;i++)
+	{
+		printf("\n %d",a);
+		c=a+b;
+		a=b;
+		c=b;
+	}
+	printf("\n %d",c);
+	return 0;
+}
