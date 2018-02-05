430fa00dfad0ea0ba6d74bea67b3606ff3cb63ad
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-05 15:57:46 +0530

    sort.c

---
+++ sort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b[5],i;
+	printf("value");
+	scanf("%d",&a);
+	printf("\narray elmts");
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&b[5]);
+		if(b[5]<a)
+		{
+		 n=0;
+		 n=a+1;
+		 n1=a+2;
+		}
+	}
+	return 0;
+}
