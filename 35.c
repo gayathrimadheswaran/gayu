210afc8d7c0fb88457a8eb5a0fe51ceb09c30d0e
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-07 16:16:55 +0530

    35.c

---
+++ 35.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char s;
+int a;
+printf("\nenter string");
+scanf("%s",s);
+printf("\n enter value");
+scanf("%d",&a);
+if(a>99)
+{
+printf("\n 1 ");
+}
+else
+{
+printf("\n 0");
+}
+return 0;
+}
