fe13d243c0e92072efff5684d280a283e1154874
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-09 16:18:01 +0530

    44.c

---
+++ 44.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("\nvalues");
+	scanf("%d",&n);
+	if(n<=10)
+	{
+		printf("\n yes");
+	}
+	else if(n>=11)
+	{
+		printf("\n no");
+	}
+	else
+	{
+		printf("\n");
+	}
+	return 0;
+}
