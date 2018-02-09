dbde8bba686c992fe6eb921ba30a24f9f5929284
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-09 16:03:53 +0530

    43.c

---
+++ 43.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[25],s2[25];
+	printf("\n string 1");
+	scanf("%s",s1);
+	printf("\n string 2");
+	scanf("%s",s2);
+	printf("\n result is %s%s",s1,s2);
+	return 0;
+}
+	
