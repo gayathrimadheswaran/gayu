2af481112d7a2df46f3b450b08f622daeb6d8a55
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-09 15:52:54 +0530

    42.c

--- 42.c
+++ 42.c
@@ -1,1 +1,27 @@
 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int n1,n2;
+	char s1[25],s2[25];
+	printf("\n string 1");
+	scanf("%s",s1);
+	printf("\n string 2");
+	scanf("%s",s2);
+	n1=strlen(s1);
+	n2=strlen(s2);
+	if(n1>n2)
+	{
+		printf("\n%s",s1);
+	}
+	else if(n2>n1)
+	{
+		printf("\n%s",s2);
+	}
+	else
+	{
+		printf("\n%s",s2);
+	}
+	return 0;
+}
