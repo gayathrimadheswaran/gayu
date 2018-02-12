687fd1e37f625bc426aa73fd2d7d7259adea5c8d
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-12 16:47:19 +0530

    53.c

--- 53.c
+++ 53.c
@@ -8,8 +8,11 @@ int main()
 	      for(i=0;i<n;i++)
 	      {
 		      scanf("%d",&a[i]);
+	      }
+	      for(i=0;i<n;i++)
+	      {
 		      sum=sum+a[i];
+	      }
 		      printf("\nresult %d",sum);
 		      return 0;
-	      }
 	      }
