d060cfc12cc810cc8f19dff45fe496a596594205
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-21 15:36:57 +0530

    57.c

---
+++ 57.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void swap(int a,int b);
+void main()
+{
+int a,b;
+clrscr();
+printf("\n values:");
+scanf("%d%d",&a,&b);
+swap(a,b);
+getch();
+}
+void swap(int a,int b)
+{
+int temp;
+temp=a;
+a=b;
+b=temp;
+}
