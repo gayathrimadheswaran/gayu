5ed19971e06789b38643a771a076a7163f4c88fb
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-23 16:42:32 +0530

    68.c

---
+++ 68.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n values");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
