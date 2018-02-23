74cf2de0d937aa4b87a950be565f0f2bf59b761f
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-23 16:51:39 +0530

    62.c

---
+++ 62.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+unsigned s;
+clrscr();
+printf("\nstring:");
+scanf("%i",&s);
+if(s%10==0||s%10==1)
+{
+printf("\nyes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
