cbb6e1ce92ab256e227077fe3063b721cd873fc1
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-28 15:52:41 +0530

    71.c

---
+++ 71.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[10];
+int rev=0,t;
+printf("\nString:");
+scanf("%s",s);
+t=s[a];
+while(t!='\0\)
+{
+rev=rec*10;
+rev=rev+t%10;
+t=t\10;
+}
+if(t==s)
+{
+printf("\nprime");
+}
+else
+{
+prime("\n not prime");
+}
+getch();
+
+}
