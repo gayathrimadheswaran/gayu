4cd0f7e40b0a6081b45814d1ab0f5b3667386d51
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-28 16:03:08 +0530

    72.c

---
+++ 72.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[10];
+printf("\n string");
+scanf("%s",s);
+if(s==a'||s=='e'||s=='i'||s=='o'||s=='u')
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
