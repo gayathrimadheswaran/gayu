7722d39e750d955613b4f0c24f4e7206e6049fdd
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-28 16:34:51 +0530

    78.c

---
+++ 78.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\nval:");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("\nyes");
+}
+else
+{
+printf("\ no");
+}
+getch();
+}
