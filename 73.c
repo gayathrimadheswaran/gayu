92ef3402e095658750c65fddafe84941d22b0bf7
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-02-28 16:09:00 +0530

    73.c

---
+++ 73.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,l,r;
+clrscr();
+for(i=o;i<n;i++)
+{
+printf("\n elmts");
+scanf("%d",&n[i]);
+if(l<n[i]&&r>n[i])
+{
+printf("\n values %d%d",l,r);
+}
+getch();
+}
