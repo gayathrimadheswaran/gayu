6ac01457c711a36545827001ff2893b9d4d3d78d
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-03-02 16:17:10 +0530

    79.c

---
+++ 79.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+int n1,n2,p,p1;
+clrscr();
+printf("\n values:");
+p=n1*n2;
+for(i=0;i<=p/2;i++)
+{
+p1=i*i;
+if(p1==p)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+}
+getch();
+}
+
