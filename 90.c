ebbdb3d623641bc1cc928dedfcaabe38430fb17c
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-03-09 16:29:03 +0530

    90.c

--- 90.c
+++ 90.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<string.h>
-void main()
+int main()
 {
-char a[10];
-int i;
-int n;
-scanf("%s",&a[i]);
+char b[10];
+int j;
+int o;
+scanf("%s",&b[j]);
 
-n=strlen(a);
-for(i=0;i<n;i++)
+o=strlen(b);
+for(j=0;j<o;j++)
 {
-    if((a[i]=='0')||(a[i]=='1')||(a[i]=='2')||(a[i]=='3')||(a[i]=='4')||(a[i]=='5')||(a[i]=='6')||(a[i]=='7')||(a[i]=='8')||(a[i]=='9'))
+    if((b[j]=='0')||(b[j]=='1')||(b[j]=='2')||(b[j]=='3')||(b[j]=='4')||(b[j]=='5')||(b[j]=='6')||(b[j]=='7')||(b[j]=='8')||(b[j]=='9'))
 {
-printf("%c",a[i]);
+printf("%c",b[j]);
 }
 }
+return 0;
 }
