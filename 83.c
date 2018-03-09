b87650dbd84f5a6b0bd36a77992aa756effb40df
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-03-09 15:57:01 +0530

    83.c

---
+++ 83.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a1,a2,a3;
+int b1,b2,b3;
+int c1,c2,c3;
+clrscr();
+scanf("%d%d",&a1,&a2);
+a3=a1/a2;
+printf("%d",a3);
+scanf("%d%d",&b1,&b2);
+b3=b1%b2;
+printf("%d",b3);
+scanf("%d%d",&c1,&c2);
+c3=c1/c2;
+printf("%d",c3);
+getch();
+}
