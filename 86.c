1f3c7730e9ac9c7866de56b9894c69e61c45d5f0
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-03-09 16:12:08 +0530

    86.c

---
+++ 86.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[10];
+int i,x=1,j;
+scanf("%s",&a[i]);
+for(i=0;s[i]!='\0';i++)
+{
+    for(j=1;s[j]!='\0';j++)
+
+{
+if(s[i]==s[j])
+{
+x++;
+}
+else
+{
+x=1;
+}
+}
+}
+if(x==1)
+{
+printf("isogram");
+}
+else
+{
+printf("no isogram");
+}
+return 0;
+}
