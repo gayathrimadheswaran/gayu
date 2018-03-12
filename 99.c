bff01db9e172e184a4cc09bae210932b42356ffc
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-03-12 16:11:22 +0530

    99.c

---
+++ 99.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a1,b1,c1,d1;
+clrscr();
+printf("enter 3 numbers:\n");
+scanf("%d %d %d",&a1,&b1,&c1);
+printf("result is :%d",a1*b1%c1);
+getch();
+}
