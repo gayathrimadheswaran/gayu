b90926a84623ca434f6ab60653040cf752d7f9bb
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-03-14 16:24:49 +0530

    108.c

---
+++ 108.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a1,b1,c1,i,sum=0;
+printf("enterelements");
+scanf("%d",&a1);
+printf("enter strting el");
+scanf("%d",&b1);
+printf("enter dif");
+scanf("%d",&c1);
+for(i=0;i<a;i++)
+{
+sum=sum+b1;
+b1=b1+c1;
+}
+printf("%d",sum);
+getch();
+}
