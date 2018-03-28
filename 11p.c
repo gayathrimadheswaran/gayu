7cee444e5bdd1872800ef8a0e68540dc049c555b
gayathrimadheswaran <30551066+gayathrimadheswaran@users.noreply.github.com>
2018-03-28 16:26:45 +0530

    11p.c

---
+++ 11p.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+	char s[50];
+	scanf("%s",s);
+	if(s[0]=='S' || s[0]=='s')
+	{
+		printf("\nyes");
+	}
+	else
+	{
+		printf("\nno");
+	}
+	getch();
+}
+ 
